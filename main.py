676c6ed37462f55f8e68932f64f420e7d1e562dd
singja <46438529+singja@users.noreply.github.com>
2019-10-29 18:02:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-def hello_world_test(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!- cloudbuild-gae-test'
+# [START app]
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!- Cloud build!' * 100
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
+# [END app]
