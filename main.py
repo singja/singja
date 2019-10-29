e4b0123745323a19287fb7dab6bf857d88732b09
singja <46438529+singja@users.noreply.github.com>
2019-10-29 16:59:11 +0900

    cloud-build-gae-helloworld

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def hello_world_test(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello World!-private'
+        return f'Hello World!- cloudbuild-gae-test'
